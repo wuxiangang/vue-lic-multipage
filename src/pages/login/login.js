e951b049e5d4a7c4ed0c8fa12b710f0d5e7f4672
wuxiangang <wuxiangang@bitboolean.com>
2018-11-28 13:37:07 +0800

    close webpack-server-dev

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -13,12 +13,11 @@ new Vue({
     this.$fetch({
       api: 'internationCode'
     })
-
     this.$fetch({
       api: 'refund',
       params: {
-        access_key: "xu2I7vMfaqDfLm5n9uHXkp1zjWu8t1IQM9H4HUkeiEwBA8DF4Jft41E4Dt8mFEpp3WMTU9944J2X2K48TACjUtaOKdi38JOgCSjiiQ6H8Gew2iz5zy83P9U4",
-        amount: '2',
+        access_key: 'xu2I7vMfaqDfLm5n9uHXkp1zjWu8t1IQM9H4HUkeiEwBA8DF4Jft41E4Dt8mFEpp3WMTU9944J2X2K48TACjUtaOKdi38JOgCSjiiQ6H8Gew2iz5zy83P9U4',
+        amount: '5',
         coin_name: 'BTC',
         user_type: 1
       }
